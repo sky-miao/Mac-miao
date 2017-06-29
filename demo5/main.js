4622bb5dc4ade887a9e7fc3f9344362f5755e5b5
mengmiao <mengmiaomiao@asean-go.com>
2017-06-29 11:14:09 +0800

    add demo5

---
+++ demo5/main.js
@@ -0,0 +1,7 @@
+var img = document.createElement('img')
+img.src = require('./4.jpg')
+document.bady.appendChild(img)
+
+var img1 = document.createElement('img')
+img1.src = require('./3.jpg')
+document.bady.appendChild(img1)
